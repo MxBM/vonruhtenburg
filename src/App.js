31e1da0d4dd020b262ce72891c18fc14af8a6e3a
officialsmp305 <Officialsmp305@gmail.com>
2022-07-19 01:12:37 -0700

    [FRONTEND]: Integrate Routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './styles/App.scss';
+import Header from './components/Header';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="container">
+          <div className="wrapper">
+            <div className="home">
+              <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/opportunities" element={<Opportunities />} />
+                <Route exact path="/solutions" element={<Solutions />} />
+                <Route exact path="/contact-us" element={<Contact />} />
+              </Routes>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
+function Opportunities() {
+  return <p>Discover our numerous opportunities</p>;
+}
+
+function Solutions() {
+  return <p>Solutions that help you.</p>;
+}
+
+function Contact() {
+  return <p>Feel free to reach us.</p>;
+}
+
+function Home() {
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <h5>
+          <b>Vonruhtenberg</b>, placeholder text placeholder text placeholder text placeholder text
+          placeholder text placeholder text placeholder text
+        </h5>
+      </div>
+    </div>
+  );
+}
 export default App;
